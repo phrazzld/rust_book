b27d7bd6da166902760566f9f39f836c2acd9aae
Phaedrus <phraznikov@gmail.com>
2019-12-14 14:08:35 -0800

    2. Programming a Guessing Game: Getting Started

---
+++ projects/guessing_game/target/debug/deps/guessing_game-1de33a86458917e7.d
@@ -0,0 +1,5 @@
+/Users/phaedrus/Dev/rust_book/projects/guessing_game/target/debug/deps/guessing_game-1de33a86458917e7: src/main.rs
+
+/Users/phaedrus/Dev/rust_book/projects/guessing_game/target/debug/deps/guessing_game-1de33a86458917e7.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/phaedrus/Dev/rust_book/projects/guessing_game/target/debug/guessing_game: /Users/phaedrus/Dev/rust_book/projects/guessing_game/src/main.rs
