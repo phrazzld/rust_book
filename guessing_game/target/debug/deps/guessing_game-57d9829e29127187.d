bf854fdfef9825cd576c7675db6c8feca8f201a2
Phaedrus <phraznikov@gmail.com>
2019-12-14 18:08:30 -0800

    2. Programming a Guessing Game: Generating random numbers

---
+++ guessing_game/target/debug/deps/guessing_game-57d9829e29127187.d
@@ -0,0 +1,5 @@
+/Users/phaedrus/Dev/rust_book/guessing_game/target/debug/deps/guessing_game-57d9829e29127187: src/main.rs
+
+/Users/phaedrus/Dev/rust_book/guessing_game/target/debug/deps/guessing_game-57d9829e29127187.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/deps/guessing_game-892214e80135aae2.d
@@ -0,0 +1,5 @@
+/Users/phaedrus/Dev/rust_book/guessing_game/target/debug/deps/guessing_game-892214e80135aae2: src/main.rs
+
+/Users/phaedrus/Dev/rust_book/guessing_game/target/debug/deps/guessing_game-892214e80135aae2.d: src/main.rs
+
+src/main.rs:
