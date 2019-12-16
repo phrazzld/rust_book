24e7135bb4bfe09e185488f1da6c6ae7d2910a4e
Phaedrus <phraznikov@gmail.com>
2019-12-15 17:33:07 -0800

    The Array Type, and Rust's Safety Principles

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let x: (i32, f64, u8) = (500, 6.4, 1);
+    let a = [1, 2, 3, 4, 5];
+    let index = 10;
 
-    let five_hundred = x.0;
+    let element = a[index];
 
-    let six_point_four = x.1;
-
-    let one = x.2;
+    println!("The value of element is: {}", element);
 }
