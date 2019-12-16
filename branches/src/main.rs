98db105302d211c497c928ec6951cb7e913a71d7
Phaedrus <phraznikov@gmail.com>
2019-12-16 13:51:42 -0800

    Looping Through a Collection with for

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let mut number = 3;
-
-    while number != 0 {
+    for number in (1..4).rev() {
         println!("{}!", number);
-
-        number -= 1;
     }
-
     println!("LIFTOFF!!!");
 }
