c59362af55712bc9087ea8fc39c19251a6b6b7aa
Phaedrus <phraznikov@gmail.com>
2019-12-15 17:28:17 -0800

    Numeric operations

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let x = 2.0; // f64
+    // addition
+    let sum = 5 + 10;
 
-    let y: f32 = 3.0; // f32
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    // remainder
+    let remainder = 43 % 3;
 }
