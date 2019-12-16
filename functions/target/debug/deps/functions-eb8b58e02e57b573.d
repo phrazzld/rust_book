5c1609c794994b31e089f1b9005bef7936c1d881
Phaedrus <phraznikov@gmail.com>
2019-12-15 17:38:18 -0800

    Functions

---
+++ functions/target/debug/deps/functions-eb8b58e02e57b573.d
@@ -0,0 +1,5 @@
+/Users/phaedrus/Dev/rust_book/functions/target/debug/deps/functions-eb8b58e02e57b573: src/main.rs
+
+/Users/phaedrus/Dev/rust_book/functions/target/debug/deps/functions-eb8b58e02e57b573.d: src/main.rs
+
+src/main.rs:
