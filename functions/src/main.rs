409f99013fa73cc95ef86ad0828c08d864385b9f
Phaedrus <phraznikov@gmail.com>
2019-12-16 13:41:40 -0800

    Functions with Return Values

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
-    another_function(5, 6);
+    let x = plus_one(5);
+
+    println!("The value of x is: {}", x);
 }
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
+fn plus_one(x: i32) -> i32 {
+    x + 1;
 }
