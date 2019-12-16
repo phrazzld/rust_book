d07f858b703535a29f774aa8a91fcf0e7387586a
Phaedrus <phraznikov@gmail.com>
2019-12-16 13:37:43 -0800

    Function parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    println!("Hello, world!");
-
-    another_function();
+    another_function(5, 6);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32, y: i32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
